6363ee7487d9255bd1954dd621a2b6c33e442b90
fahadalijs <fahad9827022@gmail.com>
2024-04-26 16:56:44 +0500

    Numbers-Methods

---
+++ app.js
@@ -0,0 +1,53 @@
+// console.log(Number-mehods);
+
+// round
+// ceil
+// floor
+// random
+// parseInt
+// parseFloat
+
+
+
+
+
+
+// round 
+
+// let num = 2.9;
+// console.log(Math.round(num));
+
+// ceil
+
+// let num = 2.1
+// console.log(Math.ceil(num));
+
+
+
+
+
+
+
+// floor
+
+// const num = 1.9;
+// console.log(Math.floor(num));
+
+
+
+
+// random
+
+// console.log(Math.ceil(Math.random()*100));
+// console.log(Math.ceil(Math.random()*10));
+
+
+
+
+
+
+
+
+
+
+
